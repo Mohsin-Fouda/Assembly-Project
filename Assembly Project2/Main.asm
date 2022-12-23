8c479f437cee6c677fcc28ffa1cb5a530049d090
F0UDA <medo2172002a@gmail.com>
2022-12-23 16:17:36 +0200

    the last

--- Assembly Project2/Main.asm
+++ Assembly Project2/Main.asm
@@ -2,21 +2,22 @@ include Irvine32.inc
 .DATA
 
 ground1 BYTE ".,---,---..-.--+--.,,-..,_.--,--'`,---..-.--+--.,,-,,..._.--...._.-.__...,..,___.--,--'`,---..-.--+--.,,-,,..._.--....",0
+uground db "+--.,,-..,_.--,--'`,---..-.--++--.,,-..,_.--,--'`,---..-.--++--.,,-..,_.--,--'`,---..-.--++--.,,-..,_.--,--'`,---..-.---",0
 
 es6            db "   ->  Your Score is:            ########        ##          ##    ##      ########                           ", 0
 es7            db "                                ##      ##      ####         ###  ###      ##     #                           ", 0
 es8            db "                               ###       #      # ##        ## ## # ##     ##                                 ", 0
 es9            db "   ->  Play Again?             ###             ##  ##       ##  ##  ##     #####                              ", 0
-es10           db "                               ###    ####     ######       ##  ##  ##     ##                                 ", 0
-es11           db "       Press Space             ###      ##    ##   ##      ##        ##    ##                                 ", 0 
+es10           db "       Press Space             ###    ####     ######       ##  ##  ##     ##                                 ", 0
+es11           db "                               ###      ##    ##   ##      ##        ##    ##                                 ", 0 
 es12           db "                                ###     ##    ##    ##     ##        ##    ##     #                           ", 0
 es13           db "                                 ########    ###    ###   ####      ####   ########                           ", 0
-es14           db "                                                                                                              ", 0
-es15           db "                                                                                                              ", 0
+es14           db "   ->  press ESC to                                                                                           ", 0
+es15           db "       exit the game                                                                                          ", 0
 es16           db "                                 #######      ###      ###    ########    ########                            ", 0
 es17           db "                                ##     ###     ##      ##     ##     #    ##     ##                           ", 0
 es18           db "                               ##       ###    ##      ##     ##          ##      #                           ", 0
-es19           db "            ################   ##       ###    ##     ##      #####       ##    ###    ################       ", 0
+es19           db "                               ##       ###    ##     ##      #####       ##    ###                           ", 0
 es20           db "                               ##       ###     ##    ##      ##          #######                             ", 0
 es21           db "                               ##       ###     ##   ##       ##          ##   #                              ", 0
 es22           db "                                ##     ###        ####        ##     #    ##    ##                            ", 0
@@ -26,20 +27,45 @@ es27           db "							                                                   			
 es28           db "                                                                                                              ", 0
 es29           db "                                                                                                              ", 0
 es30           db "																                                                 ", 0
-es31           db "                                                                                                              ", 0
+es31           db "                                                                                                                ", 0
 amount db 99
 score db "Score : ",0
 scorecounter dd 0,0
 x BYTE 20	
 y BYTE 28 ; edited
 inputchar BYTE "h"
-isJumping BYTE 'F'
 
+
+welcome1 db "Welcome To T-Rex Game ! " ,0
+welcome2 db " Press Any Key To Start The Game , press Space To Jump." ,0
+thb1   db "############################################################ ", 0
+thb2   db "#                                                          # ", 0
+thb3   db "#      By : Ibrahim Amasha & Mohsin Fouda                  # ", 0
+thb4   db "#                                                          # ", 0
+thb5   db "#      Special Thanks To Dr Sara Elsayed Elmetwally        # ", 0
+thb6   db "#                                                          # ", 0
+thb7   db "############################################################ ", 0
 
 
 
 .CODE
 MAIN PROC	
+
+;----------------------print a welcome message----------------------------
+	mov dl , 47
+	mov dh , 15
+	call Gotoxy
+	lea dx , welcome1
+	call WriteString
+ 
+	mov dl , 30
+	mov dh , 18
+	call Gotoxy
+	lea dx , welcome2
+	call WriteString
+	call readchar
+
+call Clrscr
 
 
 
@@ -59,13 +85,34 @@ call WriteString
 
 call dp
 call DrawCollision
-	
+
+
+
+
 	gameLoop:						;make collision move:
 
 			cmp amount,99
 			jge onGround2
 			call UpdateCollison
 			dec amount
+
+
+			mov dl , 0
+			mov dh , 29
+			call Gotoxy
+			lea dx , ground1
+			call WriteString	
+			mov eax , 1
+			call delay 
+			
+			mov dl , 0
+			mov dh , 29
+			call Gotoxy
+			lea dx , uground
+			call WriteString
+
+
+
 			cmp amount , 21			;making the collison stop at player to check if it jumped 
 			je therest
 			call DrawCollision
@@ -76,6 +123,23 @@ call DrawCollision
 		
 		
 	onground2:
+
+;------------------------------starting the game again if the player pressed space---------------------------
+
+		gamestart: 
+		mov dl , 80
+		mov dh , 15
+		call gotoxy
+		lea dx , score
+		call WriteString
+		call dp
+		mov dl , 0
+		mov dh , 29
+		call Gotoxy
+		lea dx , ground1
+		call WriteString
+
+
 
 		 	call UpdateCollison
 			mov amount , 50			; making the collision move back to its initial place
@@ -118,13 +182,133 @@ there20:
 		mov  eax,50          ; sleep, to allow OS to time slice
 		call Delay           ; (otherwise, some key presses are lost)
 		call ReadKey    
-		cmp al , 'x'
+		cmp al , 20h ; if the user input key us space then the player jumps
 		je KeepGoing
 		jne EndScreen
+
+
+
+		
+;----------------------------------------------------
+
+
+KeepGoing:
+		call dp2
+		call up1
+		mov eax ,250
+		call delay
+		call dp
+		call up2
+
+		mov dl , 88
+		mov dh , 15
+		call gotoxy
+
+		mov inputchar,al
+		inc scorecounter
+		mov eax,scorecounter 
+        call WriteDec
+
+jumpLoop:
+		mov y , 20				; how high the jump is
+
+								; making the ground move
+		mov ecx , 3				; how long the ground moves , and the player is in air
+		mov eax ,100
+		call delay
+
+myloop:
+		call DrawGround1
+		dec x
+		mov eax , 30
+		call delay
+		cmp x , 0
+		je starting
+		jne rest 
+
+starting:
+
+		mov x , 5
+
+rest:
+		loop myloop
+		call UpdateCollison
+
+gravity5:
+
+		cmp y , 23			
+		jge onGround5
+		call UpdateCollison
+		dec amount
+
+;--------------------- checking to end the game ------------------------------
+		cmp amount , 20
+		je there
+		jne theree
+there:
+
+		mov inputchar,al
+		cmp al , "x"
+
+theree:
+
+
+		cmp amount , 20				; making the collision stop at x = 0 and get back to its initial plcae / edited
+		je onground2
+		mov bl , y
+		mov ecx , 8
+
+
+l:
+
+ 		inc y 
+		loop l
+
+		mov y , bl
+		call DrawCollision
+		mov eax , 20
+		call delay
+		jmp gravity5
+
+
+onGround5:
+
+		mov eax , 500
+		call delay
+		call UpdateCollison
+		dec amount
+		call DrawCollision
+		mov ecx , 10 
+
+jumploop5:							;allow player to jump
+
+		call UpdateCollison
+		dec amount
+		call DrawCollision
+		mov eax ,20
+		call delay
+		loop jumploop5
+
+
+
+
 
 ;----------------------   End Screen Label   -----------------------------
 
 
+
+		mov inputchar,al
+		call ReadChar 
+		cmp al , ' '
+		je  U
+		jne J
+		U:	
+		call UpdateEndScreen
+		J:
+
+
+
+		
 EndScreen:
 
 mov dl , 0
@@ -271,6 +455,11 @@ call gotoxy
 mov edx , OFFSET es31
 call WriteString
 Call UpdateDrawGround1
+call up1
+call dp
+
+
+
 
 ;----------------------- Display Score Counter  -----------------------------
 mov dl ,22
@@ -279,120 +468,69 @@ call gotoxy
 mov eax,scorecounter
 call WriteDec	
 call readchar
+cmp al , 20h ; if player pressed space the game would start agian
+je F
+jne exitGame
 
-		mov inputchar,al
-		call ReadKey    
-		cmp al , ' '
-		je  UpdateEndScreen
-		
+F:
+mov scorecounter , 0
+call Clrscr
+;call dp
+cmp al , 20h
+je gamestart
 
+exitGame:
 
+	call Clrscr
 
+mov dl , 30
+mov dh , 14
+call gotoxy
+lea dx , thb1
+call WriteString
 
-		
-;----------------------------------------------------
+mov dl , 30
+mov dh , 15
+call gotoxy
+lea dx , thb2
+call WriteString
 
+mov dl , 30
+mov dh , 16
+call gotoxy
+lea dx , thb3
+call WriteString
 
-KeepGoing:
-		call dp2
-		call up1
-		mov eax ,250
-		call delay
-		call dp
-		call up2
+mov dl , 30
+mov dh , 17
+call gotoxy
+lea dx , thb4
+call WriteString
 
-		mov dl , 88
-		mov dh , 15
-		call gotoxy
+mov dl , 30
+mov dh , 18
+call gotoxy
+lea dx , thb5
+call WriteString
 
-		mov inputchar,al
-		inc scorecounter
-		mov eax,scorecounter 
-        call WriteDec
+mov dl , 30
+mov dh , 19
+call gotoxy
+lea dx , thb6
+call WriteString
 
-jumpLoop:
-		mov y , 20				; how high the jump is
-
-								; making the ground move
-		mov ecx , 3				; how long the ground moves , and the player is in air
-		mov eax ,100
-		call delay
-
-myloop:
-		call DrawGround1
-		dec x
-		mov eax , 30
-		call delay
-		cmp x , 0
-		je starting
-		jne rest 
-
-starting:
-
-		mov x , 5
-
-rest:
-		loop myloop
-		call UpdateCollison
-
-gravity5:
-
-		cmp y , 23			
-		jge onGround5
-		call UpdateCollison
-		dec amount
-
-;--------------------- checking to end the game ------------------------------
-		cmp amount , 20
-		je there
-		jne theree
-there:
-
-		mov inputchar,al
-		cmp al , "x"
-
-theree:
+mov dl , 30
+mov dh , 20
+call gotoxy
+lea dx , thb7
+call WriteString
 
 
-		cmp amount , 20				; making the collision stop at x = 0 and get back to its initial plcae / edited
-		je onground2
-		mov bl , y
-		mov ecx , 8
-
-
-l:
-
- 		inc y 
-		loop l
-
-		mov y , bl
-		call DrawCollision
-		mov eax , 20
-		call delay
-		jmp gravity5
-
-
-onGround5:
-
-		mov eax , 500
-		call delay
-		call UpdateCollison
-		dec amount
-		call DrawCollision
-		mov ecx , 10 
-
-jumploop5:							;allow player to jump
-
-		call UpdateCollison
-		dec amount
-		call DrawCollision
-		mov eax ,20
-		call delay
-		loop jumploop5
+	
+	call readchar
 
 
 
-	exitGame:
 	exit
 main ENDP
 
@@ -422,9 +560,16 @@ UpdateDrawGround1 proc
 
 UpdateDrawGround1 endp
 
+;---------------------- Draw Collision proc -------------------------
 
 
 DrawCollision PROC
+
+	mov dl, amount
+	mov dh,28
+	call gotoxy
+	mov al , 0ddh
+	CALL WriteChar
 
 	mov dl, amount
 	mov dh,27
@@ -446,12 +591,6 @@ DrawCollision PROC
 
 	mov dl, amount
 	mov dh,24
-	call gotoxy
-	mov al , 0ddh
-	CALL WriteChar
-
-	mov dl, amount
-	mov dh,23
 	call gotoxy
 	mov al , 0ddh
 	CALL WriteChar
@@ -461,9 +600,16 @@ DrawCollision ENDP
 
 
 
+;---------------------- Update Collison proc -------------------------
 
 
 UpdateCollison PROC
+
+	mov dl,amount 
+	mov dh,28
+	call gotoxy
+	mov al," "
+	call writechar
 
 	mov dl,amount 
 	mov dh,27
@@ -489,21 +635,76 @@ UpdateCollison PROC
 	mov al," "
 	call writechar
 
-	mov dl,amount 
-	mov dh,23
-	call gotoxy
-	mov al," "
-	call writechar
-
 	ret
 UpdateCollison ENDP
 
- 
 
-
-
+;----------------------  Draw Player proc -------------------------
 
 dp proc 
+;------------------------- zero line-------------------------------
+
+	mov dl,8
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
+	mov dl,9
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
+	mov dl,10
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+
+	mov dl,11
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+
+	mov dl,12
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
+	mov dl,13
+	mov dh,23
+	call gotoxy
+	mov al," "
+
+	CALL WriteChar	
+
+	mov dl,14
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+
+	mov dl,15
+	mov dh,23
+	call gotoxy
+	mov al,"_"
+	CALL WriteChar
+	
+	mov dl,16
+	mov dh,23
+	call gotoxy
+	mov al,"_"
+	CALL WriteChar
+		
+	mov dl,17
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
 
 ;-------------------------first line-------------------------------
 
@@ -820,8 +1021,72 @@ dp endp
 
 
 up1 proc 
+;------------------------- zero line-------------------------------
+
+	mov dl,8
+	mov dh,24
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
+	mov dl,9
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
+	mov dl,10
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+
+	mov dl,11
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+
+	mov dl,12
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
+	mov dl,23
+	mov dh,24
+	call gotoxy
+	mov al," "
+
+	CALL WriteChar	
+
+	mov dl,23
+	mov dh,24
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+
+	mov dl,15
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
+	mov dl,16
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+		
+	mov dl,17
+	mov dh,23
+	call gotoxy
+	mov al," "
+	CALL WriteChar
+	
 
 ;-------------------------first line-------------------------------
+	
 
 	mov dl,8
 	mov dh,24
